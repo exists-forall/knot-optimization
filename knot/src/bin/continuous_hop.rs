28346098d846e4321513d3e568829a2dbd5e2978
William Brandon <hypercube97@gmail.com>
2018-04-19 17:36:00 +0000

    Extend continuous_hop to support importing from files

--- knot/src/bin/continuous_hop.rs
+++ knot/src/bin/continuous_hop.rs
@@ -1,21 +1,27 @@
 extern crate alga;
 extern crate nalgebra;
+extern crate serde_json;
 
 extern crate knot;
 
 use std::f64::consts::PI;
 use std::f64::INFINITY;
+use std::fs::File;
+use std::process::exit;
+use std::env::args;
 
 use nalgebra::{UnitQuaternion, Vector3};
 use alga::general::SubsetOf;
 
 use knot::geometries::curve_9_40;
-use knot::continuous_optimize::RepulsionChain;
 use knot::defaults::continuous_optimization::{COST_PARAMS, RATE, STEPS, REPULSION,
                                               REPULSION_EXPONENT, REPULSION_STRENGTH,
                                               MAX_REPULSION_STRENGTH, CURVE_9_40_CHAIN_SIZE,
                                               RETURN_TO_INITIAL_WEIGHT, RETURN_TO_INITIAL};
-use knot::symmetry::symmetries;
+use knot::symmetry::{symmetries, symmetries_with_skip};
+use knot::continuous_optimize::{Chain, Leg, PhantomJoint, RepulsionChain};
+use knot::report::{KnotGeometry, Transform, JointsParity};
+use knot::isometry_adjust;
 
 const TAU: f64 = 2.0 * PI;
 
@@ -38,21 +44,79 @@ fn optimize(chain: &mut RepulsionChain, steps: u32) -> f64 {
 }
 
 fn main() {
-    let mut best_chain = RepulsionChain::new(
-        curve_9_40::chain(
-            CURVE_9_40_CHAIN_SIZE,
-            1.0,
-            COST_PARAMS,
-            RETURN_TO_INITIAL_WEIGHT,
-            RATE,
-        ),
-        symmetries(3).map(|quat| quat.to_superset()).collect(),
-        REPULSION_EXPONENT,
-        REPULSION_STRENGTH,
-        MAX_REPULSION_STRENGTH,
-    );
+    let (mut best_chain, symms, parity) = match args().nth(1) {
+        Some(filename) => {
+            let file = File::open(&filename).unwrap_or_else(|_| {
+                eprintln!("Could not open file {}", filename);
+                exit(1);
+            });
+            let geometry: KnotGeometry = serde_json::from_reader(file).unwrap_or_else(|_| {
+                eprintln!("Could not parse input file");
+                exit(1);
+            });
+            (
+                RepulsionChain::new(
+                    Chain::new(
+                        geometry.joint_spec,
+                        geometry.num_angles,
+                        PhantomJoint {
+                            symmetry: geometry.symmetries[1].to_isometry(),
+                            index: 0,
+                            leg: Leg::Incoming,
+                        },
+                        // post-phantom
+                        PhantomJoint {
+                            symmetry: geometry.symmetries[3].to_isometry(),
+                            index: geometry.transforms.len() - 1,
+                            leg: Leg::Outgoing,
+                        },
+                        geometry.cost_params,
+                        RETURN_TO_INITIAL_WEIGHT,
+                        RATE / 10.0,
+                        isometry_adjust::Steps::new_uniform(0.000001),
+                        geometry
+                            .transforms
+                            .iter()
+                            .map(Transform::to_isometry)
+                            .collect(),
+                    ),
+                    geometry
+                        .symmetries
+                        .iter()
+                        .map(Transform::to_isometry)
+                        .collect(),
+                    REPULSION_EXPONENT,
+                    REPULSION_STRENGTH,
+                    MAX_REPULSION_STRENGTH,
+                ),
+                geometry.symmetries,
+                geometry.parity,
+            )
+        }
+        None => {
+            (
+                RepulsionChain::new(
+                    curve_9_40::chain(
+                        CURVE_9_40_CHAIN_SIZE,
+                        0.7,
+                        COST_PARAMS,
+                        RETURN_TO_INITIAL_WEIGHT,
+                        RATE,
+                    ),
+                    symmetries(3).map(|quat| quat.to_superset()).collect(),
+                    REPULSION_EXPONENT,
+                    REPULSION_STRENGTH,
+                    MAX_REPULSION_STRENGTH,
+                ),
+                symmetries_with_skip(3, 4)
+                    .map(|iso| Transform::from_isometry(iso.to_superset()))
+                    .collect(),
+                JointsParity::Even,
+            )
+        }
+    };
     let mut best_cost = optimize(&mut best_chain, STEPS);
-    println!("Original cost: {}", best_cost);
+    eprintln!("Original cost: {}", best_cost);
 
     let mut steps = Vec::new();
 
@@ -60,7 +124,7 @@ fn main() {
         let mut new_best = None;
         let mut new_best_cost = best_cost;
 
-        println!("Best cost by epoch {}: {}", epoch, best_cost);
+        eprintln!("Best cost by epoch {}: {}", epoch, best_cost);
 
         for i in 0..best_chain.joints.len() {
             for &offset in &[-2.0, -1.0, 1.0, 2.0] {
@@ -74,9 +138,9 @@ fn main() {
                 offset_chain.joints[i] = offset_chain.joints[i] *
                     UnitQuaternion::from_axis_angle(&Vector3::y_axis(), angle);
                 let cost = optimize(&mut offset_chain, STEPS);
-                println!("{} {:+}: {}", i, offset, cost);
+                eprintln!("{} {:+}: {}", i, offset, cost);
                 if cost < new_best_cost {
-                    println!("Best so far!");
+                    eprintln!("Best so far!");
                     new_best = Some((offset_chain, i, offset));
                     new_best_cost = cost;
                 }
@@ -84,18 +148,37 @@ fn main() {
         }
 
         if let Some((new_best_chain, new_best_i, new_best_offset)) = new_best {
-            println!("New best: {} {:+}", new_best_i, new_best_offset);
+            eprintln!("New best: {} {:+}", new_best_i, new_best_offset);
             steps.push((new_best_i, new_best_offset));
             best_chain = new_best_chain;
             best_cost = new_best_cost;
         } else {
-            println!("Couldn't find a better offset");
+            eprintln!("Couldn't find a better offset");
             break;
         }
     }
 
-    println!("\nFinal steps:");
+    eprintln!("\nFinal steps:");
     for &(i, offset) in &steps {
-        println!("{} {:+}", i, offset);
+        eprintln!("{} {:+}", i, offset);
     }
+
+    let transforms = best_chain
+        .joints
+        .iter()
+        .cloned()
+        .map(|iso| Transform::from_isometry(iso))
+        .collect::<Vec<_>>();
+
+    let geometry = KnotGeometry {
+        joint_spec: best_chain.spec,
+        num_angles: best_chain.num_angles,
+        cost_params: best_chain.cost_params,
+        parity: parity,
+        symmetries: symms,
+        transforms,
+    };
+
+    eprintln!("\nFinal geometry:");
+    println!("{}", serde_json::to_string_pretty(&geometry).unwrap());
 }
