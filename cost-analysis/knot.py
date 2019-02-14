6b05496af8b9283da93db2637d3efda187dbf79f
Jonathan Liu <liujon23@gmail.com>
2019-02-13 21:19:52 -0800

    ressults fully updated

--- cost-analysis/knot.py
+++ cost-analysis/knot.py
@@ -1,3 +1,9 @@
+import numpy as np
+import json
+
 # Knot Object
 class Knot:
-    def __init__(self, angles, final_angle, )
+    def __init__(self, angles, final_angle, cost):
+        self.angles = angles
+        self.angles.append(round(final_angle))
+        self.cost = cost
