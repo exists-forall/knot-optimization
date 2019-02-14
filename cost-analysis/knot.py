a3f357700f2db624f6fff3e512e28c35ff815607
liujon23 <liujon23@gmail.com>
2019-02-14 00:39:32 -0800

    Knot reading works
    We can now convert json files of knots into a set of knots. Working on creating sets of adjacent knots.

--- cost-analysis/knot.py
+++ cost-analysis/knot.py
@@ -1,28 +1,29 @@
 import json
 
+
 # The fundamental knot object.
 class Knot:
-    def __init__(self, angles, final_angle, cost, parity):
-        self.angles = angles
-        self.angles.append(round(final_angle))
-        self.cost = cost
-        self.parity = parity
-
     def __init__(self, angles, cost, parity):
         self.angles = angles
         self.cost = cost
         self.parity = parity
 
+
 # Set of knots with some functionality for identifying other knots.
 class KnotSet:
     def __init__(self, json_file):
         # Matrix with knots sorted by parity.
-        self.knots = [[] for _ in range(16)]
-
         with open(json_file) as f:
             data = json.load(f)
+
+        self.total_parity = data["num_angles"]
+
+        self.knots = [[] for _ in range(self.total_parity)]
+
         for knot in data["knots"]:
-            temp_knot = Knot(knot["angles"], knot["final_angle"], knot["total_cost"], knot["angle_parity"])
+            temp_angles = knot["angles"]
+            temp_angles.append(round(knot["final_angle"]))
+            temp_knot = Knot(knot["angles"], knot["total_cost"], knot["angle_parity"])
             self.knots[temp_knot.parity].append(temp_knot)
 
     # Returns a list of knot objects exactly one "move" away from current knot.
@@ -30,33 +31,26 @@ class KnotSet:
         adj_knots = []
         for i in range(len(knot.angles)):
             new_angles = knot.angles.copy()
-            new_angles[i] += 1
-            if (i == len(knot.angles) - 1):
-                new_angles[0] -= 1
+            new_angles[i] = (new_angles[i] + 1) % self.total_parity
+            if i + 1 == len(knot.angles):
+                new_angles[0] = (new_angles[0] - 1) % self.total_parity
             else:
-                new_angles[i + 1] -= 1
-            for parity_match in self.knots[knot.parity]:
-                if parity_match.angles == new_angles:
-                    adj_knots.append(parity_match)
-                else:
-                    bad_knot = Knot(angles, 2, knot.parity)
-                    adj_knots.append(bad_knot)
+                new_angles[i+1] = (new_angles[i+1] - 1) % self.total_parity
 
-            new_angles = knot.angles.copy()
-            new_angles[i] -= 1
-            if (i == len(knot.angles) - 1):
-                new_angles[0] += 1
-            else:
-                new_angles[i + 1] += 1
+            found = False
             for parity_match in self.knots[knot.parity]:
                 if parity_match.angles == new_angles:
                     adj_knots.append(parity_match)
-                else:
-                    bad_knot = Knot(angles, 2, knot.parity)
-                    adj_knots.append(bad_knot)
+                    found = True
+
+            if not found:
+                bad_knot = Knot(new_angles, 3, knot.parity)
+                adj_knots.append(bad_knot)
+
         return adj_knots
-        
-# Returns the number of moves requred to convert one knot to another knot.
+
+
+# Returns the number of moves required to convert one knot to another knot.
 # Requires the two knots to be of the same parity.
 def distance(knot1, knot2):
     pass
--- cost-analysis/test.py
+++ cost-analysis/test.py
@@ -1,3 +1,12 @@
+import knot
+
+
 def main():
-    knot_set = KnotSet("trefoil_statistics/top_100.json")
-    print(knot_set.knots[10])
+    knot_set = knot.KnotSet("trefoil_statistics/top_100.json")
+    print(knot_set.knots[10][0].cost)
+    adj_set = knot_set.adjacent_knots(knot_set.knots[10][0])
+    for adj_knot in adj_set:
+        print(adj_knot.angles)
+
+
+main()
